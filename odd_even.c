22da15337549ae6aa66556a22bc5ea0f312aa483
Vidhi524 <147084357+Vidhi524@users.noreply.github.com>
2023-11-03 19:33:35 +0530

    Create odd_even

---
+++ odd_even.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+int n;
+printf("Enter a Number \n");
+scanf("%d",&n);
+    if(n%2==0)
+    {
+        printf("\n %d is even",n);
+    }
+    else
+    {
+        printf("\n %d is odd",n);
+    }
+
+    return 0;
+
+}
