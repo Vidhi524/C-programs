3d941223f9ec65e6f5a586ef083658c3bcdbc4d1
Vidhi524 <147084357+Vidhi524@users.noreply.github.com>
2023-11-09 11:25:31 +0530

    Create pattern_i

---
+++ pattern_i.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+     printf("\n");
+      printf("\n");
+    for(int i=1;i<=5;i++)
+    {
+     for(int j=1;j<=5;j++)   
+    if(i==1 || i==5 || j==3)
+    {
+        printf("*");
+    }
+    else
+    {
+        printf(" ");
+    }
+        printf("\n");
+    }
+
+     printf("\n");
+      printf("\n");
+    return 0;
+}
