64c4e056a2a07035c836b72f44fdb71c6852b86d
Vidhi524 <147084357+Vidhi524@users.noreply.github.com>
2023-11-23 11:16:14 +0530

    Create transposematrix.c

---
+++ transposematrix.c
@@ -0,0 +1,44 @@
+int r,c; // m for 
+//     printf("Enter No. of Rows of matrix \n");
+//     scanf("%d",&r);
+//     printf("Enter No. of Columns of matrix \n");
+//     scanf("%d",&c);
+//     int a[r][c];
+//     printf("Enter Array Elements \n");
+// for(int i=0;i<r;i++)
+// {
+//     for(int j=0;j<c;j++)
+//     {
+//         scanf("%d",&a[i][j]);
+
+//     }
+// }
+// printf("Matrix is:  \n");
+// for(int i=0;i<r;i++)
+// {
+//     for(int j=0;j<c;j++)
+//     {
+//         printf("%d  ",a[i][j]);
+//     }
+//     printf("\n");
+// }
+// int at[c][r];
+
+// printf("Transpose of given matrix is: \n");
+
+// for(int i=0;i<c;i++)
+// {
+//     for(int j=0;j<r;j++)
+//     {
+//         at[i][j]=a[j][i];
+//     }
+// }
+
+// for(int i=0;i<c;i++)
+// {
+//     for(int j=0;j<r;j++)
+//     {
+//         printf("%d  ",at[i][j]);
+//     }
+//     printf("\n");
+// }
