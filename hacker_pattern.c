bdc8906a79c3e8889b38953f8f5c824c45c278a1
Vidhi524 <147084357+Vidhi524@users.noreply.github.com>
2023-12-25 21:29:49 +0530

    Add files via upload
    Pattern printing

---
+++ hacker_pattern.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n;
+    printf("Enter Number\n");
+    scanf("%d",&n);
+    for(int i=1;i<=(2*n-1);i++)
+    {
+        for(int j=1;j<=(2*n-1);j++)
+        {
+            
+        }
+printf("\n");
+    }
+return 0;
+}
